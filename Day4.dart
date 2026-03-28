78ef368b641ec0d84ca51e13faabf440c7bd7c32
Amina Ali <adniyaali@gmail.com>
2026-03-28 14:56:46 +0500

    Day4practice.dart

--- Day4.dart
+++ Day4.dart
@@ -1,4 +1,6 @@
-
+Day 4 : Interface,Mixin,Enum
+  Author:https://github.com/thedevamina
+//interface
 abstract class Chargeable{
   void charge();
 }
@@ -35,12 +37,122 @@ void swimmable(){
 print("$name can swim");
 }
 }
+//mixin
+mixin CarDrive{
+  void drive(){
+
+    print("can drive");
+  
+  }}
+  mixin Fly{
+   void flying(){ 
+    print("Flying");
+  }
+}
+
+class Driver with CarDrive,Fly{
+  String name;
+  Driver(this.name);
+}
+
+
+
+mixin CanShoot{
+  void shoot(){
+    print("Shoooting");
+  }
+}
+mixin CanRun{
+  void run(){
+    print("Running");
+  }
+}
+class PUBGPlayer with CanShoot,CanRun,CarDrive{
+String name;
+PUBGPlayer(this.name);
+}
+
+//enum for car brands
+enum CarBrand{
+  BMW, Mercidies,aulto,mehran
+}
+//for game status
+enum GameStatus{
+  Playing,
+  Pause,
+   gameOver
+}
+
+//for food status
+enum FoodStatus{
+  ordered, 
+  preparing,delivered,cancelled
+}
+//for player rank
+enum PlayerRank{
+  bronze, silver,gold,platinum,diamond
+
+}
 void main(){
+   print("Here are interface:");
   Duck d1 = Duck("SMARK");
   d1.flyable();
   d1.swimmable();
   PlayStation p1 = PlayStation("GTA5");
   p1.charge();
  p1.play();
+
+ //mixin
+ print("Here are mixins:");
+Driver d2= Driver("XYZ");
+d2.drive();
+d2.flying();
+
+PUBGPlayer player = PUBGPlayer("Ali");
+player.shoot();
+player.drive();
+player.run();
+
+
+//enum 
+
+print("here are enum:");
+
+CarBrand myCar = CarBrand.aulto;
+print(myCar.name);
+
+GameStatus myStatus = GameStatus.Playing;
+if(myStatus == GameStatus.Playing){
+ print("Game is Running");
+
+}else if(myStatus == GameStatus.Pause){
+  print("Game is Paused");
+}else{
+  print("Game is Over");
+}
+
+FoodStatus fs = FoodStatus.ordered;
+if(fs==FoodStatus.ordered){
+  print("Food is Ordered");
+}else if(fs == FoodStatus.delivered){
+  print("Food is Delivered");
+}else{
+  print("Order is Cancelled");
+}
+
+PlayerRank pr = PlayerRank.bronze;
+if(pr == PlayerRank.bronze){
+  print("Bronze");
+}else if(pr == PlayerRank.diamond){
+  print("Diamond");
+}else if(pr == PlayerRank.silver){
+  print("Silver");
+  }else if(pr == PlayerRank.gold){
+  print("Gold");
+  }
+
+  else{
+  print("Platinum");
+}
 }
 
